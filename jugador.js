6fdaed70d70f4f68d531be984b8f4ad4897bce16
mrucal <rucalgr@gmail.com>
2019-11-17 00:48:55 +0100

    Arreglados bugs menores

--- jugador.js
+++ jugador.js
@@ -4,7 +4,7 @@ class Jugador{
         this.nombre = nombre;
         this.puntos = 0;
         this.id_putnos = "#"+id;
-        $("#name_"+id).html(nombre);
+        $("#name_"+id).html(nombre+$("#name_"+id).html());
     }
 
     incrementarPuntos(){
